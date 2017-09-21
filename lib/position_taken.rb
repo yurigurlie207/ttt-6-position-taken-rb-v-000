8dc2acffc788acfc072796dee09997a69767cd55
Adrienne Yao <adrienne.yao@gmail.com>
2017-09-21 16:50:12 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board,number)
+
+  if board[number] == " " || board[number] == "" || board[number] == nil
+    return false
+  else
+    return true
+  end
+end
